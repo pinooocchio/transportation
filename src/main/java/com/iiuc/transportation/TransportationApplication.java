3cacef150027704d6d27e7ff93efc8eb7ec9b724
Nabir <mohammadalamnabir80@gmail.com>
2026-05-22 23:39:37 +0600

    Update TransportationApplication.java

--- src/main/java/com/iiuc/transportation/TransportationApplication.java
+++ src/main/java/com/iiuc/transportation/TransportationApplication.java
@@ -1,13 +1,1 @@
-package com.iiuc.transportation;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TransportationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TransportationApplication.class, args);
-	}
-
-}
